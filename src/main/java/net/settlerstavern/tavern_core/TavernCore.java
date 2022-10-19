76e2eaca0a06b48a7301215b40376f04a98764bd
Luke Ireton <lr.ireton@gmail.com>
2022-10-19 13:47:00 +0100

    refactored tags to use common tags

--- src/main/java/net/settlerstavern/tavern_core/TavernCore.java
+++ src/main/java/net/settlerstavern/tavern_core/TavernCore.java
@@ -1,13 +1,19 @@
 package net.settlerstavern.tavern_core;
+import net.minecraft.item.Item;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntryList;
 import net.settlerstavern.tavern_core.block.ModBlocks;
 import net.fabricmc.api.ModInitializer;
 import net.settlerstavern.tavern_core.block.ModBlocks_Trees;
 import net.settlerstavern.tavern_core.item.ModItems;
 import net.settlerstavern.tavern_core.util.ModFlammableBlocks;
 import net.settlerstavern.tavern_core.util.ModStrippableBlocks;
+import net.settlerstavern.tavern_core.util.ModTags;
 import net.settlerstavern.tavern_core.world.feature.ModConfiguredFeatures;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 public class TavernCore implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -27,6 +33,9 @@ public class TavernCore implements ModInitializer {
 		ModBlocks_Trees.registerModBlocks();
 		ModFlammableBlocks.registerFlammableBlocks();
 		ModStrippableBlocks.registerStrippables();
+
+		Optional<RegistryEntryList.Named<Item>> tagList = Registry.ITEM.getEntryList(ModTags.Items.FOSSILS);
+		System.out.println("Tag list items: " + tagList);
 	}
 
 }
--- src/main/java/net/settlerstavern/tavern_core/util/ModTags.java
+++ src/main/java/net/settlerstavern/tavern_core/util/ModTags.java
@@ -6,9 +6,18 @@ import net.minecraft.item.Item;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.settlerstavern.tavern_core.TavernCore;
 
 public class ModTags {
     public static class Items {
-        public static final TagKey<Item> FOSSILS = TagKey.of(Registry.ITEM_KEY, new Identifier("tavern_core", "fossils"));
+        public static final TagKey<Item> FOSSILS = createCommonTag("fossils");
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(Registry.ITEM_KEY, new Identifier(TavernCore.MOD_ID, name));
+        }
+
+        private static TagKey<Item> createCommonTag(String name) {
+            return TagKey.of(Registry.ITEM_KEY, new Identifier("c", name));
+        }
     }
 }
